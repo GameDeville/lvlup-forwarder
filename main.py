366dd4549a5068dba3af7f5270dc95c0543893a2
GameDeville <gamedeville@gmail.com>
2025-07-16 17:37:38 +0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters
 import os
 
-api_id = int(os.getenv("25960539"))
-api_hash = os.getenv("264cc6d76bda37f314bf4349581aa451")
-source_channel = os.getenv("stopgameru")  # Пример: "@stopgamenews"
-target_channel = os.getenv("@lvlupnws")  # Пример: "@lvlupnws"
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+source_channel = os.getenv("SOURCE_CHANNEL")  # Пример: "@stopgamenews"
+target_channel = os.getenv("TARGET_CHANNEL")  # Пример: "@lvlupnws"
 
 app = Client("forwarder", api_id=api_id, api_hash=api_hash)
 
